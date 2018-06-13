83d366d38344b64d4a913f9d0566181198ddfc2e
ironkey <hcb0402@gmail.com>
2018-06-14 02:02:28 +0900

    4장

---
+++ Week 2/P118_magic8Ball2.py
@@ -0,0 +1,13 @@
+import random
+
+messages = ['It is certain',
+            'It is decidedly so',
+            'Yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concetrate and ask again',
+            'My replay is no',
+            'Outlook not so good',
+            'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
