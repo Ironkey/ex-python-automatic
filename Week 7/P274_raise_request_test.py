7beb1f5bd682ed04a4422609c05ff0a273c54216
Ironkey <hcb0402@gmail.com>
2018-08-10 18:50:10 +0900

    11장 진행

--- Week 7/P274_raise_request_test.py
+++ Week 7/P274_raise_request_test.py
@@ -4,4 +4,4 @@ res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
 try:
     res.raise_for_status()
 except Exception as exc:
-    print('There was a prblem: %s' % (exc))
+    print('There was a problem: %s' % (exc))
