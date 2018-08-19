b315d02aadedbd4771c1158ffc2099ec073757e8
ironkey <hcb0402@gmail.com>
2018-08-19 17:45:21 +0900

    12장 진행

--- Week 7/P302_Command_Email_Submit.py
+++ Week 7/P302_Command_Email_Submit.py
@@ -1,20 +1,26 @@
 #! python3
-import sys
+import sys, time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 if len(sys.argv) > 3:
     email = sys.argv[1]
     text = sys.argv[2]
+
 else:
     print('Usage : Command [email] [text]')
 
 browser = webdriver.Chrome(r'C:\Users\ChiHoon\Downloads\chromedriver\chromedriver.exe')
 browser.get('http://gmail.com')
 emailElem = browser.find_element_by_id('identifierId')
-emailElem.send_keys('hcb0402')
-IdNext = browser.find_element_by_id('identifierNext').click()
-#passwordElem = browser.find_element_by_name('password')
-passwordElem = browser.find_element_by_class_name('OabDMe cXrdqd Y2Zypf')
-passwordElem.send_keys('djflq77clf!')
-PasswordNext = browser.find_element_by_id('passwordNext').click()
+emailElem.send_keys('id')
+browser.find_element_by_id('identifierNext').click()
+time.sleep(1)
+passwordElem = browser.find_element_by_name('password')
+passwordElem.send_keys('password')
+browser.find_element_by_id('passwordNext').click()
+time.sleep(1)
+browser.get('https://mail.google.com/mail/u/0/#inbox?compose=new')
+time.sleep(1)
+browser.find_element_by_name('to').send_keys('test@test.com')
+browser.find_element_by_name('subjectbox').send_keys('test')
+browser.find_element_by_name('textbox').send_keys('testtest')
